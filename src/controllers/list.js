a8daa5a185b1e930c0f8906656860928bc1f488a
Kennedy Daniel <kenidankid@yahoo.com>
2023-08-27 15:40:56 +0100

    delete to do from the database

--- src/controllers/list.js
+++ src/controllers/list.js
@@ -20,15 +20,40 @@ exports.createToDos = async (req, res) => {
 
   exports.updateTodos = async(req, res) => {
     try {
+      //get the params using req.params
+
          const todosId = req.params.todo
+         //find the particular data you want to delete via the id, 
+
         const updatedToDos = await Lists.findOneAndUpdate(
           { _id: todosId },
+          //the new data that will replace the old data 
           {todolist: req.body.todolist},        
           { new: true }
         );
         console.log(updatedToDos);
   
         res.status(200).json(updatedToDos);
+      // }
+  
+     
+    } catch (error) {
+      console.log(error);
+    }
+  } 
+
+  exports.deleteTodos = async(req, res) => {
+    try {
+      //get the params using req.params
+         const todosId = req.params.todo
+
+         //find the particular data you want to delete via the id
+        const deletedToDos = await Lists.findOneAndDelete(
+          { _id: todosId },
+        );
+   
+  
+        res.status(200).json(deletedToDos);
       // }
   
      
--- src/routes/list.js
+++ src/routes/list.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { createToDos, updateTodos } = require("../controllers/list");
+const { createToDos, updateTodos, deleteTodos } = require("../controllers/list");
 
 router.post("/create", createToDos);
 router.put("/update/:todo" ,updateTodos) ;
+router.delete("/delete/:todo" ,deleteTodos) ;
 
 
 module.exports = router;
